9c513c02a3a276451cad19481eee2df3110936f8
Felipe Gutierrez <fgutierrez3@wisc.edu>
2017-02-20 22:59:47 -0600

    Add sub-sampling rate check. Make sure that it is not too low.

--- Example_TwoSampleRapidPT.m
+++ Example_TwoSampleRapidPT.m
@@ -4,17 +4,19 @@ addpath(RapidPTLibraryPath);
 
 % Load input data and input labels
 dataPath = '../50_25_25.mat'; 
+% dataPath = '../snpm_test_data_14.mat'; 
 load(dataPath);
+
 % N subjects, V voxels (or statistics)
 [N,V] = size(Data);
-numPermutations = 5000;
-nGroup1 = 25; % You should what is the size of one of your groups prior.
+numPermutations = 3000;
+nGroup1 = 7; % You should what is the size of one of your groups prior.
 % Set write to 1 if you want the matrices used to recover the permutation matrix.
 % Setting this to 1 will make outputs a very large variable, but may be
-% useful in certain cases.
+% useful for some analysis
 write = 0;
 
 [outputs, timings] = TwoSampleRapidPT(Data, numPermutations, nGroup1, write, RapidPTLibraryPath);
 
-save(strcat('outputs/outputs_',num2str(numPermutations),'.mat'),'outputs');
-save(strcat('timings/timings_',num2str(numPermutations),'.mat'),'timings');
+% save(strcat('../outputs_',num2str(numPermutations),'.mat'),'outputs');
+% save(strcat('../timings_',num2str(numPermutations),'.mat'),'timings');
