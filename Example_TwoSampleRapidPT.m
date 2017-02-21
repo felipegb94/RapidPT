16edce1fdf6861fb203b84f5a0122ccf63e58ad6
Felipe Gutierrez <fgutierrez3@wisc.edu>
2017-02-21 00:38:36 -0600

    change to gpl lic

--- Example_TwoSampleRapidPT.m
+++ Example_TwoSampleRapidPT.m
@@ -2,15 +2,17 @@
 RapidPTLibraryPath = '.';
 addpath(RapidPTLibraryPath);
 
+
+rng(1);
 % Load input data and input labels
-dataPath = '../50_25_25.mat'; 
-% dataPath = '../snpm_test_data_14.mat'; 
+% dataPath = '../50_25_25.mat'; 
+dataPath = '../snpm_test_data_14.mat'; 
 load(dataPath);
-% Data = X;
+Data = X;
 % N subjects, V voxels (or statistics)
 [N,V] = size(Data);
-numPermutations = 5000;
-nGroup1 = 25; % You should what is the size of one of your groups prior.
+numPermutations = 3000;
+nGroup1 = 7; % You should what is the size of one of your groups prior.
 % Set write to 1 if you want the matrices used to recover the permutation matrix.
 % Setting this to 1 will make outputs a very large variable, but may be
 % useful in certain cases.
